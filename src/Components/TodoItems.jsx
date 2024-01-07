c21b412e734f042b2a9003d317383c8866d52084
acz19 <acz1992@hotmail.co.uk>
2024-01-07 20:49:50 +0100

    Add functionality

---
+++ src/Components/TodoItems.jsx
@@ -0,0 +1,17 @@
+import "./CSS/Todoitems.css";
+import tick from "./Assets/tick.png";
+import not_tick from "./Assets/not_tick.png";
+import cross from "./Assets/cross.png";
+
+export const TodoItems = ({ no, display, text }) => {
+	return (
+		<div className="todoitems">
+			<div className="todoitems-container">
+				<img src={not_tick} alt="" />
+				<img src={tick} alt="" />
+				<div className="todoitems-text">{text}</div>
+			</div>
+			<img className="todoitems-cross-icon" src={cross} alt="" />
+		</div>
+	);
+};
